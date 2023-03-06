048fccbf89201c59987cf9cf1a9a64254ce2b5ed
최서윤 <cinnamon1995@naver.com>
2023-03-06 18:02:07 +0900

    함수형

--- src/App.js
+++ src/App.js
@@ -9,10 +9,21 @@ function App() {
       <div>Number State : {number}</div>
       <button
         onClick={() => {
-          setNumber(number + 1)
+          // setNumber(number + 1)
+          setNumber((currentNumber) => {
+            return currentNumber + 1
+          })
+          //함수형
         }}
       >
         up
+      </button>
+      <button
+        onClick={() => {
+          setNumber(number - 1)
+        }}
+      >
+        down
       </button>
     </>
   )
