eff343cdc1f330337799ecdbf8468518df4f204e
최서윤 <cinnamon1995@naver.com>
2023-03-06 18:07:18 +0900

    usestate 함수형 + 둘의 차이

--- src/App.js
+++ src/App.js
@@ -10,10 +10,17 @@ function App() {
       <button
         onClick={() => {
           // setNumber(number + 1)
+          //기존 배치성으로 처리돼서 , 배치업데이트
           setNumber((currentNumber) => {
             return currentNumber + 1
           })
-          //함수형
+          setNumber((currentNumber) => {
+            return currentNumber - 2
+          })
+          setNumber((currentNumber) => {
+            return currentNumber + 3
+          })
+          //함수형 3개만들면 한번에 +3 됨 명령들을 모아 순차적으로 한번씩 작동됨
         }}
       >
         up
